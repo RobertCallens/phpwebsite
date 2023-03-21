248a7a5282488d12b3f02756fbe13aee09bf51e4
Robert Callens <ByteGamerYT@gmail.com>
2023-03-21 11:25:07 +0100

    push

--- shoutbox.php
+++ shoutbox.php
@@ -2,6 +2,12 @@
 include 'includes/header.php';
 include 'database/connectie.php'
 ?>
+<?php
+
+    $query = "SELECT * FROM shoutbox";
+    $berichten = mysqli_query($con, $query);
+
+?>
     <main>
         <div class="uk-card uk-card-default uk-width-1-2@m uk-align-center">
             <div class="uk-card-header">
@@ -12,27 +18,45 @@ include 'database/connectie.php'
                 </div>
             </div>
             <div class="uk-card-body">
-                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2023-03-07T11:00"> 7-03-2023 - 11:00 - </time><strong>naam: </strong>Dag allemaal</p>
-                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2023-03-07T11:00"> 7-03-2023 - 11:00 - </time><strong>naam: </strong>Dag allemaal</p>
-                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2023-03-07T11:00"> 7-03-2023 - 11:00 - </time><strong>naam: </strong>Dag allemaal</p>
-                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2023-03-07T11:00"> 7-03-2023 - 11:00 - </time><strong>naam: </strong>Dag allemaal</p>
+                <ul class="uk-list">
+                    <?php
+                        while($rij = mysqli_fetch_assoc($berichten)):
+                    ?>
+                    <li>
+                        <span class="uk-text-meta uk-margin-remove-top"><?php echo $rij['tijd'] ?> - </span>
+                        <span class="uk-text-bold"><?php echo $rij['gebruiker'] ?>: </span>
+                        <span class=".uk-text-normaal"><?php echo $rij['bericht'] ?></span>
+                    </li>
+                    <?php endwhile;?>
+                </ul>
             </div>
             <div class="uk-card-footer uk-card-primary">
-                <form>
-                    <div class="uk-margin">
+                <?php
+                    if(isset($_GET['fout'])): ?>
+                        <div class="uk-text-warning uk-text-right uk-animation-fade">
+                            <?php echo $_GET['fout']; ?>
+                        </div>
+                    <?php endif; ?>
+
+    
+                <form method="post" action="shoutbox/process.php">
+                    <div class="uk-margin"> 
                         <div class="uk-inline">
                             <span class="uk-form-icon" uk-icon="icon: user"></span>
-                            <input class="uk-input" type="text" placeholder="Voornaam" aria-label="Not clickable icon">
+                            <input class="uk-input" type="text" placeholder="Voornaam" name="gebruiker" aria-label="Not clickable icon">
                         </div>
+                    </div>
                         <div class="uk-margin">
                             <div class="uk-inline">
                                 <span class="uk-form-icon" uk-icon="icon: file-edit"></span>
-                                <input class="uk-input uk-form-width-large" type="text" placeholder="Bericht" aria-label="Not clickable icon">
+                                <input class="uk-input uk-form-width-large" type="text" placeholder="Bericht" name="bericht" aria-label="Not clickable icon">
                             </div>
-                    </div>
-                        <div>
-                            <input type="button" class="uk-button uk-button-danger" value="SCHREEUW HET UIT!!">
                         </div>
+                    <div class="uk-margin">
+                        <div class="uk-inline">
+                            <input type="submit" name="verzenden" class="uk-button uk-button-danger" value="SCHREEUW HET UIT!!" aria-label="Not clickable icon">
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
